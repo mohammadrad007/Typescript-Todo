8a95619b27abe277cd527677dd38c09cf7f8e428
mohammad.dev.007 <mohammad.mohamadi0101@gmail.com>
2022-03-07 15:48:46 +0330

    Interface Model ... Model.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputFeild from "./components/InputFeild";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   return (
     <div className="App">
